3f4da025dacdadca551df9427ef1b065bd1e6fb3
Mohanraj B [mohanrajb] <c_mohanrajb+costco@costco.com>
2026-05-08 19:23:49 +0530

    updates

--- marketing-site/server.js
+++ marketing-site/server.js
@@ -7,27 +7,28 @@ const PORT = process.env.PORT || 5501;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const ROOT_DIR = process.env.VERCEL ? process.cwd() : __dirname;
 
-app.use(express.static(__dirname));
+app.use(express.static(ROOT_DIR));
 
 app.get("/", (_req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "index.html"));
+  res.sendFile(path.join(ROOT_DIR, "pages", "index.html"));
 });
 
 app.get("/resume", (_req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "resume.html"));
+  res.sendFile(path.join(ROOT_DIR, "pages", "resume.html"));
 });
 
 app.get("/portfolio", (_req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "portfolio.html"));
+  res.sendFile(path.join(ROOT_DIR, "pages", "portfolio.html"));
 });
 
 app.get("/blog", (_req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "blog.html"));
+  res.sendFile(path.join(ROOT_DIR, "pages", "blog.html"));
 });
 
 app.get("/contact", (_req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "contact.html"));
+  res.sendFile(path.join(ROOT_DIR, "pages", "contact.html"));
 });
 
 app.listen(PORT, () => {
